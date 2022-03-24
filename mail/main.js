7158fedc845023808b55f6eb21758cf9bbd57e05
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-03-24 13:32:33 +0100

    dichiarazione variabili e scorrimento array ciclo

---
+++ mail/main.js
@@ -0,0 +1,5 @@
+let emailUtente=prompt("Inserisci email valida");
+let listaEmail=["matteo@gmail.com" , "fabio@gmail.com" , "pietro@gmail.com" , "anna@gmail.com"];
+for(let i=0 ; i<listaEmail.length ; i++){
+    
+}
