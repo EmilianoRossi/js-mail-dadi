e426e942bde8687b002dfbd76ca8e713ee2f0637
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-03-24 14:38:37 +0100

    validazione email inserita con ciclo for

--- mail/main.js
+++ mail/main.js
@@ -1,5 +1,19 @@
 let emailUtente=prompt("Inserisci email valida");
 let listaEmail=["matteo@gmail.com" , "fabio@gmail.com" , "pietro@gmail.com" , "anna@gmail.com"];
-for(let i=0 ; i<listaEmail.length ; i++){
-    
+/*for(let i=listaEmail.length ; i < listaEmail.length ; i++){
+    if(emailUtente==listaEmail[i]){
+        alert(listaEmail[i]);
+        alert("Email presente");
+    }else{
+        alert("email non presente");
+    }
 }
+*/
+
+for(let i= 0 ; i< listaEmail.length; i++){
+    if(listaEmail[i]===emailUtente){
+        console.log("Email accettata");
+        break;
+    }
+}
+        
